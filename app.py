d9be4b8df19142ac98227b44856c56be443169be
AthulyaN <athulya.24@gmail.com>
2021-12-08 09:54:32 -0500

    Reformatted

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-#A Flask server app implementing an API that interfaces to an SQLite DB
-#Basic HTTP error codes have been implemented
-#TODO - Exception handling
+# A Flask server app implementing an API that interfaces to an SQLite DB
+# Basic HTTP error codes have been implemented
+# TODO - Exception handling
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -9,10 +9,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-#Defining the model for Table Movie
+
+# Defining the model for Table Movie
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable = False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     language = db.Column(db.String(80), nullable=False)
     genre = db.Column(db.String(80), nullable=False)
     rating = db.Column(db.Float)
@@ -20,47 +21,52 @@ class Movie(db.Model):
     def __repr__(self):
         return f"{self.name} - {self.language} - {self.genre} - {self.rating}"
 
+
 @app.route('/')
 def hello():
     return 'Hello!'
 
-#For GET request to http://localhost:5000/movies
+
+# For GET request to http://localhost:5000/movies
 @app.route('/movies')
 def get_movies():
     movies = Movie.query.all()
-    movie_list =[]
+    movie_list = []
     for movie in movies:
-        movie_data = {'Id':movie.id, 'Name':movie.name, 'Language':movie.language,
-                      'Genre':movie.genre, 'IMDB rating': movie.rating}
+        movie_data = {'Id': movie.id, 'Name': movie.name, 'Language': movie.language,
+                      'Genre': movie.genre, 'IMDB rating': movie.rating}
         movie_list.append(movie_data)
-    return {"Movies" : movie_list}, 200
+    return {"Movies": movie_list}, 200
 
-#For GET request to http://localhost:5000/movies/<id>
+
+# For GET request to http://localhost:5000/movies/<id>
 @app.route('/movies/<id>')
 def get_movie(id):
     movie = Movie.query.get_or_404(id)
-    return jsonify({'Id':movie.id, 'Name':movie.name, 'Language':movie.language,
-                    'Genre':movie.genre, 'IMDB rating': movie.rating})
+    return jsonify({'Id': movie.id, 'Name': movie.name, 'Language': movie.language,
+                    'Genre': movie.genre, 'IMDB rating': movie.rating})
 
-#For POST request to http://localhost:5000/movies
-@app.route('/movies', methods = ['POST'])
+
+# For POST request to http://localhost:5000/movies
+@app.route('/movies', methods=['POST'])
 def add_movie():
     if request.is_json:
         movie = Movie(name=request.json['Name'], language=request.json['Language'],
                       genre=request.json['Genre'], rating=request.json['IMDB rating'])
         db.session.add(movie)
         db.session.commit()
-        return jsonify({'Id':movie.id, 'Name':movie.name, 'Language':movie.language,
-                        'Genre':movie.genre, 'IMDB rating': movie.rating}), 201
+        return jsonify({'Id': movie.id, 'Name': movie.name, 'Language': movie.language,
+                        'Genre': movie.genre, 'IMDB rating': movie.rating}), 201
     else:
         return {'error': 'Request must be JSON'}, 400
 
-#For DELETE request to http://localhost:5000/movies/<id>
+
+# For DELETE request to http://localhost:5000/movies/<id>
 @app.route('/movies/<id>', methods=['DELETE'])
 def delete_movie(id):
     movie = Movie.query.get(id)
     if movie is None:
-        return {'error':'not found'}, 404
+        return {'error': 'not found'}, 404
     db.session.delete(movie)
     db.session.commit()
     return f'{id} is deleted', 200
